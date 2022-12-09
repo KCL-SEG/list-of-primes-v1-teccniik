cc2f8d27de53e2b52cc57865d6717fb212ab2f8c
Jude Lawrence <jude.lawrence@kcl.ac.uk>
2022-12-09 14:45:10 +0000

    Solved

--- primes.py
+++ primes.py
@@ -3,4 +3,21 @@
 
 def primes(number_of_primes):
     list = []
+    number =2
+
+    while len(list) < number_of_primes:
+        is_prime = True
+
+        for prime in list:
+            if number % prime ==0:
+                is_prime = False
+                break
+
+        if is_prime:
+            list.append(number)
+
+        number += 1
+
+
+
     return list
